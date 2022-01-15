717b00c5ce7f851b87731676b477b3025858422a
actuallyfro <actuallyfro@github.com>
2022-01-15 08:37:05 -0600

    {startPythonServer} For testing

---
+++ startPythonServer.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#From: https://developer.mozilla.org/en-US/docs/Learn/Common_questions/set_up_a_local_testing_server
+
+# If Python version returned above is 3.X
+# On Windows, try "python -m http.server" or "py -3 -m http.server"
+python3 -m http.server
+
+## If Python version returned above is 2.X
+#python -m SimpleHTTPServer
