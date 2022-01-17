e497ac1dfaee0008a76c74ccf8aa30548bf4d803
actuallyfro <actuallyfro@github.com>
2022-01-17 15:32:37 -0600

    {BASH_generate_skcsv_str.sh} Framed in MOST of the needed logic code -- fill in the blanks!

--- 01_Helper_Tools/BASH_generate_skcsv_str.sh
+++ 01_Helper_Tools/BASH_generate_skcsv_str.sh
@@ -6,8 +6,77 @@ if [ -z "$1" ]; then
   exit 1
 fi
 
-#iterate over substring, broken up by comma
+loopCounter=0
+#iterate over substring, broken up by comm
+outStr=""
 for componentCostStr in $(echo $1 | tr "," "\n"); do
-  echo "[DEBUG] Looking at: $componentCostStr"
+  # echo "[DEBUG] Looking at: $componentCostStr"
+
+  # break componentCostStr into key and value
+  key=""
+  value=""
+  isLoopFirstDone="false"
+  isLoopSecondDone="false"
+
+  OLDIFS="$IFS"
+  IFS=';'
+  for keyPair in $componentCostStr; do
+    if [ "$isLoopFirstDone" == "false" ]; then
+      key="$keyPair"
+      isLoopFirstDone="true"
+      # echo "[DEBUG] FIRST LOOP!"
+
+    elif [ "$isLoopSecondDone" == "false" ]; then
+      value="$keyPair"
+      isLoopSecondDone="true"
+      # echo "[DEBUG] SECOND LOOP!"
+    fi 
+  done
+  IFS="$OLDIFS"
+
+  #echo "[DEBUG] Key ($key): Value($value)"
+
+  if [ "$loopCounter" == "0" ]; then
+    echo "[DEBUG] Checking material!"
+    compString=""
+
+    if [ "$key" != "m" ]; then
+      echo "[WARNING] No material provided!"
+    fi
+
+    #Material Check:
+    if [ "$value" == "bg" ]; then
+      echo "[DEBUG] Processing Bulletproof Glass"
+      compString="Bulletproof Glass" #FIX
+
+    elif [ "$value" == "ca" ]; then
+      echo "[DEBUG] Processing Canvas"
+      compString="Canvas" #FIX
+
+    elif [ "$value" == "co" ]; then
+      echo "[DEBUG] Processing Computer"
+      compString="Computer" #FIX
+
+    fi
+
+    newStr="$outStr $compString,"
+    outStr="$newStr"
+
+  else 
+    if [ "$key" != "i" ]; then
+      echo "[DEBUG] Processing Iron"
+
+    elif [ "$key" != "s" ]; then
+      echo "[DEBUG] Processing Silcon"
+
+    fi
+
+  fi
+
+
+  loopCounter=$((loopCounter + 1))
 
 done
+
+echo "RECCOMEND: "
+echo "$outStr"
