bc56e11e6acd7b8cafc896d96869d0d015ef2e8d
actuallyfro <actuallyfro@github.com>
2022-01-20 18:17:58 -0600

    {runALLComponents.sh} All real values added

--- 01_Helper_Tools/runALLComponents.sh
+++ 01_Helper_Tools/runALLComponents.sh
@@ -15,57 +15,57 @@
 ./BASH_generate_skcsv_str.sh "jc;cc,fe;8"
 
 #Detector Components
-./BASH_generate_skcsv_str.sh "jc;dc,fe;5,,ni;15"
+./BASH_generate_skcsv_str.sh "jc;dc,fe;5,ni;15"
 
 #Display
-./BASH_generate_skcsv_str.sh "jc;di,co;0,au;0,gravel;0,fe;0,mg;0,ni;0,pt;0,si;0,ag;0,u;0"
+./BASH_generate_skcsv_str.sh "jc;di,fe;1,si;5"
 
 #Explosives
-./BASH_generate_skcsv_str.sh "jc;ex,co;0,au;0,gravel;0,fe;0,mg;0,ni;0,pt;0,si;0,ag;0,u;0"
+./BASH_generate_skcsv_str.sh "jc;ex,mg;2,si;0.5"
 
 #Girder
-./BASH_generate_skcsv_str.sh "jc;gi,co;0,au;0,gravel;0,fe;0,mg;0,ni;0,pt;0,si;0,ag;0,u;0"
+./BASH_generate_skcsv_str.sh "jc;gi,fe;6"
 
 #Gravity Generator Components
-./BASH_generate_skcsv_str.sh "jc;gc,co;0,au;0,gravel;0,fe;0,mg;0,ni;0,pt;0,si;0,ag;0,u;0"
+./BASH_generate_skcsv_str.sh "jc;gc,co;2200,au;10,fe;600,ag;5"
 
 #Interior Plate
-./BASH_generate_skcsv_str.sh "jc;ip,co;0,au;0,gravel;0,fe;0,mg;0,ni;0,pt;0,si;0,ag;0,u;0"
+./BASH_generate_skcsv_str.sh "jc;ip,fe;3"
 
 #Large Steel Tube
-./BASH_generate_skcsv_str.sh "jc;lt,co;0,au;0,gravel;0,fe;0,mg;0,ni;0,pt;0,si;0,ag;0,u;0"
+./BASH_generate_skcsv_str.sh "jc;lt,fe;30"
 
 #Medical Components
-./BASH_generate_skcsv_str.sh "jc;mc,co;0,au;0,gravel;0,fe;0,mg;0,ni;0,pt;0,si;0,ag;0,u;0"
+./BASH_generate_skcsv_str.sh "jc;mc,fe;60,ni;70,ag;20"
 
 #Metal Grid
 ./BASH_generate_skcsv_str.sh "jc;mg,co;3,fe;12,ni;5"
 
 #Motor
-./BASH_generate_skcsv_str.sh "jc;mo,co;0,au;0,gravel;0,fe;0,mg;0,ni;0,pt;0,si;0,ag;0,u;0"
+./BASH_generate_skcsv_str.sh "jc;mo,fe;20,ni;5"
 
 #Power Cell
-./BASH_generate_skcsv_str.sh "jc;pc,co;0,au;0,gravel;0,fe;0,mg;0,ni;0,pt;0,si;0,ag;0,u;0"
+./BASH_generate_skcsv_str.sh "jc;pc,fe;10,ni;2,si;1"
 
 #Radio-Communication
-./BASH_generate_skcsv_str.sh "jc;rc,co;0,au;0,gravel;0,fe;0,mg;0,ni;0,pt;0,si;0,ag;0,u;0"
+./BASH_generate_skcsv_str.sh "jc;rc,fe;8,si;1"
 
 #Reactor Components
-./BASH_generate_skcsv_str.sh "jc;ec,co;0,au;0,gravel;0,fe;0,mg;0,ni;0,pt;0,si;0,ag;0,u;0"
+./BASH_generate_skcsv_str.sh "jc;ec,gravel;20,fe;15,,ag;5"
 
 #Small Steel Tube
-./BASH_generate_skcsv_str.sh "jc;st,co;0,au;0,gravel;0,fe;0,mg;0,ni;0,pt;0,si;0,ag;0,u;0"
+./BASH_generate_skcsv_str.sh "jc;st,fe;5"
 
 #Solar Cell
-./BASH_generate_skcsv_str.sh "jc;sc,co;0,au;0,gravel;0,fe;0,mg;0,ni;0,pt;0,si;0,ag;0,u;0"
+./BASH_generate_skcsv_str.sh "jc;sc,ni;3,si;6"
 
 #Steel Plate
-./BASH_generate_skcsv_str.sh "jc;sp,co;0,au;0,gravel;0,fe;0,mg;0,ni;0,pt;0,si;0,ag;0,u;0"
+./BASH_generate_skcsv_str.sh "jc;sp,fe;21"
 
 #Superconductor Component
-./BASH_generate_skcsv_str.sh "jc;uc,co;0,au;0,gravel;0,fe;0,mg;0,ni;0,pt;0,si;0,ag;0,u;0"
+./BASH_generate_skcsv_str.sh "jc;uc,au;2,fe;10"
 
 #Thruster Components
-./BASH_generate_skcsv_str.sh "jc;tc,co;0,au;0,gravel;0,fe;0,mg;0,ni;0,pt;0,si;0,ag;0,u;0"
+./BASH_generate_skcsv_str.sh "jc;tc,co;10,au;1,fe;30,pt;0.4"
 
 
