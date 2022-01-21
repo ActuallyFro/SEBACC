c2d4f8699a7082d30dd54d0e06eef6dcc1fcc443
actuallyfro <actuallyfro@github.com>
2022-01-20 18:35:27 -0600

    {genCSV.sh} Adding csv cut of mat costs, to 'hand calculate' values in Excel/Sheets

---
+++ 01_Helper_Tools/genCSV.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+#source: https://www.spaceengineerswiki.com/Category:Components
+
+#Bulletproof Glass
+./BASH_generate_skcsv_str.sh "c;bg,si;15"
+
+#Canvas
+./BASH_generate_skcsv_str.sh "c;ca,fe;2,si;35"
+
+#Computer
+./BASH_generate_skcsv_str.sh "c;co,fe;0.5,si;0.2"
+
+#Construction Component
+./BASH_generate_skcsv_str.sh "c;cc,fe;8"
+
+#Detector Components
+./BASH_generate_skcsv_str.sh "c;dc,fe;5,ni;15"
+
+#Display
+./BASH_generate_skcsv_str.sh "c;di,fe;1,si;5"
+
+#Explosives
+./BASH_generate_skcsv_str.sh "c;ex,mg;2,si;0.5"
+
+#Girder
+./BASH_generate_skcsv_str.sh "c;gi,fe;6"
+
+#Gravity Generator Components
+./BASH_generate_skcsv_str.sh "c;gc,co;2200,au;10,fe;600,ag;5"
+
+#Interior Plate
+./BASH_generate_skcsv_str.sh "c;ip,fe;3"
+
+#Large Steel Tube
+./BASH_generate_skcsv_str.sh "c;lt,fe;30"
+
+#Medical Components
+./BASH_generate_skcsv_str.sh "c;mc,fe;60,ni;70,ag;20"
+
+#Metal Grid
+./BASH_generate_skcsv_str.sh "c;mg,co;3,fe;12,ni;5"
+
+#Motor
+./BASH_generate_skcsv_str.sh "c;mo,fe;20,ni;5"
+
+#Power Cell
+./BASH_generate_skcsv_str.sh "c;pc,fe;10,ni;2,si;1"
+
+#Radio-Communication
+./BASH_generate_skcsv_str.sh "c;rc,fe;8,si;1"
+
+#Reactor Components
+./BASH_generate_skcsv_str.sh "c;ec,gravel;20,fe;15,,ag;5"
+
+#Small Steel Tube
+./BASH_generate_skcsv_str.sh "c;st,fe;5"
+
+#Solar Cell
+./BASH_generate_skcsv_str.sh "c;sc,ni;3,si;6"
+
+#Steel Plate
+./BASH_generate_skcsv_str.sh "c;sp,fe;21"
+
+#Superconductor Component
+./BASH_generate_skcsv_str.sh "c;uc,au;2,fe;10"
+
+#Thruster Components
+./BASH_generate_skcsv_str.sh "c;tc,co;10,au;1,fe;30,pt;0.4"
